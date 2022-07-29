183a8b3bd46f2b1545852732259c4da925996beb
Daniel Naschberger <daniel@naschi.at>
2022-07-29 22:11:48 +0200

    init: init ref

---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <NuxtLink to="/">index</NuxtLink><br />
+        <NuxtLink to="/test">test</NuxtLink>
+    </div>
+</template>
---
+++ pages/test.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <div>
+            <NuxtLink to="/">index</NuxtLink><br />
+            <NuxtLink to="/test">test</NuxtLink>
+        </div>
+        <input ref="input" />
+    </div>
+</template>
+
+<script setup>
+import { ref, watchEffect } from 'vue'
+
+// declare a ref to hold the element reference
+// the name must match template ref value
+const input = ref(null)
+
+watchEffect(() => {
+    if (input.value) {
+        console.log(input.value)
+        console.log(input.value?.clientWidth)
+    }
+})
+</script>
